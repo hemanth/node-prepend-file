e6d275bb6c88654671c1c7ee17ebc78268f5f209
hemanth.hm <hemanth.hm@gmail.com>
2014-06-21 18:46:04 +0530

    JSlint fixes.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 var fs = require('fs');
 
 module.exports = function(fileName, text, opts, cb) {
-    if (!text) throw new Error("No data supplied");
-    if (arguments.length == 3) {
-        if (Object.prototype.toString.call(opts) == "[object Function]") {
+    if (!text) {
+        throw new Error('No data supplied');
+    }
+    if (arguments.length === 3) {
+        if (Object.prototype.toString.call(opts) === '[object Function]') {
             cb = opts;
         } else {
-            throw new Error("Callback missing!");
+            throw new Error('Callback missing!');
         }
     }
     fs.readFile(fileName, 'utf8', function(err, data) {
@@ -15,8 +17,10 @@ module.exports = function(fileName, text, opts, cb) {
             return cb(err);
         } else {
             fs.unlinkSync(fileName);
-            fs.writeFile(fileName, text + data, function(err, data) {
-                if (err) return cb(err);
+            fs.writeFile(fileName, text + data, function(err) {
+                if (err) {
+                    return cb(err);
+                }
                 return cb(true);
             });
         }
